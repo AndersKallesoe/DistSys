eeef0cca3754fc6af6513741000ce8a7cb7b4e2e
madsflink <madsflink@live.dk>
2021-09-13 11:46:40 +0200

    still changing folder structure

---
+++ exercise-2-4-server_test.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestgetIPandPort(t *testing.T) {
+	n := mkNode()
+	n.GetIpAndPort()
+}
+
+func TestSend(t *testing.T) {
+
+}
+
+func TestListen(t *testing.T) {
+
+}
+
+func TestHandleConnection(t *testing.T) {
+
+}
+
+func TestMakeConns(t *testing.T) {
+
+}
+
+func TestPrintHostNames(t *testing.T) {
+
+}
+
+func TestPrintMap(t *testing.T) {
+
+}
