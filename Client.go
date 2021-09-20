4044e882eb71234c1b0ee5548d25799daf6624c6
madsflink <madsflink@live.dk>
2021-09-20 12:19:02 +0200

    possibly working maybe

--- Client.go
+++ Client.go
@@ -16,6 +16,7 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 )
@@ -30,6 +31,12 @@ func MakeLedger() *Ledger {
 	ledger := new(Ledger)
 	ledger.Accounts = make(map[string]int)
 	return ledger
+}
+
+func (c *Client) printLedger() {
+	for k, v := range c.ledger.Accounts {
+		fmt.Println("Account: " + k + " Balance: " + strconv.FormatInt(64, v))
+	}
 }
 
 type Transaction struct {
@@ -49,12 +56,24 @@ func (l *Ledger) Transaction(t *Transaction) {
 
 /*********************/
 
+type Message struct {
+	Msgtype     string
+	Transaction Transaction
+	IPandPort   string
+	Peers       []string
+}
+
 type Client struct {
-	ledger    *Ledger
-	peers     []string
-	conns     Conns
-	IPandPort string
-	Outbound chan struct
+	ledger       *Ledger
+	peers        []string
+	conns        Conns
+	IPandPort    string
+	index        int
+	transactions []string
+}
+
+func (c *Client) getID() string {
+	return c.IPandPort + ":" + strconv.FormatInt(int64(c.index), 64)
 }
 
 func makeClient() *Client {
@@ -62,6 +81,8 @@ func makeClient() *Client {
 	client.ledger = MakeLedger()
 	client.peers = []string{}
 	client.conns = Conns{m: make(map[string]net.Conn)}
+	client.index = 0
+	client.transactions = []string{}
 	return client
 }
 
@@ -74,6 +95,15 @@ type Conns struct {
 // Add connection to the network
 func (conns *Conns) Set(key string, val net.Conn) {
 	conns.m[key] = val
+}
+
+func (c *Client) PeerExists(peer string) bool {
+	for p := range c.peers {
+		if c.peers[p] == peer {
+			return true
+		}
+	}
+	return false
 }
 
 // Ask for <IP:Port>, read from terminal, and return it
@@ -102,20 +132,20 @@ func (c *Client) ConnectToNetwork() {
 		fmt.Println("connecting to network, requesting list of peers")
 
 		enc := gob.NewEncoder(conn)
-		request := "Requesting Peers"
-		err := enc.EncodeValue(&request)
+		request := Message{Msgtype: "Requesting Peers"}
 		fmt.Println(request)
+		err := enc.Encode(&request)
 		if err != nil {
 			fmt.Println("Encode error request:", err)
 		}
 
 		dec := gob.NewDecoder(conn)
-		peers := []string{}
-		err = dec.Decode(&peers)
+		msg := Message{}
+		err = dec.Decode(&msg)
 		if err != nil {
 			fmt.Println("Decode error in list of peers:", err)
 		}
-		c.peers = append(c.peers, peers...)
+		c.peers = append(c.peers, msg.Peers...)
 		c.peers = append(c.peers, c.IPandPort)
 		fmt.Println("peers are :", c.peers)
 		conn.Close()
@@ -127,7 +157,7 @@ func (c *Client) ConnectToNetwork() {
 func (c *Client) ConnectToPeers() {
 	//determine which peers to connect to
 	peers := c.peers
-	if len(peers) < 12 {
+	if len(peers) <= 11 {
 		peers = peers[:len(peers)-1]
 	} else {
 		peers = peers[len(peers)-11 : len(peers)-1]
@@ -140,8 +170,8 @@ func (c *Client) ConnectToPeers() {
 			return
 		} else {
 			enc := gob.NewEncoder(conn)
-			request := "Connection"
-			err := enc.Encode(&request)
+			request := Message{Msgtype: "Connection"}
+			err := enc.Encode(request)
 			if err != nil {
 				fmt.Println("Encode error request:", err)
 			}
@@ -149,6 +179,46 @@ func (c *Client) ConnectToPeers() {
 
 		}
 	}
+	//broadcast
+	c.Broadcastpresence(c.IPandPort)
+}
+
+func (c *Client) Broadcastpresence(IPAndPort string) {
+
+	for k := range c.conns.m {
+		enc := gob.NewEncoder(c.conns.m[k])
+		request := Message{Msgtype: "Broadcast presence", IPandPort: IPAndPort}
+		err := enc.Encode(request)
+		if err != nil {
+			fmt.Println("Encode error request:", err)
+		}
+	}
+}
+
+func (c *Client) BroadcastTransaction(t Transaction) {
+	if c.TransactionExists(t.ID) {
+		return
+	}
+	c.ledger.Transaction(&t)
+	c.transactions = append(c.transactions, t.ID)
+	for k := range c.conns.m {
+		enc := gob.NewEncoder(c.conns.m[k])
+		request := Message{Msgtype: "Broadcast Transaction", Transaction: t}
+		err := enc.Encode(request)
+		if err != nil {
+			fmt.Println("Encode error request:", err)
+		}
+	}
+}
+
+func (c *Client) TransactionExists(transaction string) bool {
+	for p := range c.transactions {
+		if c.transactions[p] == transaction {
+			return true
+		}
+	}
+	return false
+
 }
 
 func (c *Client) StartListen() net.Listener {
@@ -165,24 +235,29 @@ func (c *Client) Listen(ln net.Listener) {
 	for {
 		conn, _ := ln.Accept()
 		fmt.Println("Got a connection, awaiting request...")
-		request := ""
+		msg := Message{}
 		dec := gob.NewDecoder(conn)
-		err := dec.Decode(&request)
+		err := dec.Decode(&msg)
 		if err != nil {
 			fmt.Println("Decode error in msg:", err)
 		}
-		fmt.Println(request)
-		if request == "Requesting Peers" {
-			peers := c.peers
+		fmt.Println(msg)
+		switch msg.Msgtype {
+		case "Requesting Peers":
+			peers := Message{Peers: c.peers}
 			enc := gob.NewEncoder(conn)
 			err = enc.Encode(&peers)
 			if err != nil {
 				fmt.Println("Encode error in list of peers:", err)
 			}
 			conn.Close()
-		} else if request == "Connection" {
+		case "Connection":
 			c.conns.Set(conn.RemoteAddr().String(), conn)
+			go c.HandleConnection(conn)
+		default:
+			fmt.Println("No match case found for: " + msg.Msgtype)
 		}
+
 	}
 }
 
@@ -190,16 +265,28 @@ func getIP() string {
 	// _ is convention for throwing the return value away
 	name, _ := os.Hostname()
 	addrs, _ := net.LookupHost(name)
-	IP := addrs[1]
+	IP := addrs[len(addrs)-2]
 	fmt.Println("IP : " + IP)
 	return IP
 }
 
-func (c *Client) HandleConnection(){
-	
+func (c *Client) HandleConnection(conn net.Conn) {
+	for {
+		dec := gob.NewDecoder(conn)
+		msg := Message{}
+		err := dec.Decode(&msg)
+		if err != nil {
+			fmt.Println("Encode error in broadcasting presence to network:", err)
+		}
+		if !c.PeerExists(msg.IPandPort) {
+			c.peers = append(c.peers, msg.IPandPort)
+			c.Broadcastpresence(msg.IPandPort)
+		}
+
+	}
 }
 
-func (c *Client) Broadcast() {
+/*func (c *Client) Broadcast() {
 	for {
 		msg := <-n.Outbound
 		n.Conns.mutex.Lock()
@@ -208,9 +295,9 @@ func (c *Client) Broadcast() {
 		}
 		n.Conns.mutex.Unlock()
 	}
-}
+}*/
 
-func (n *Node) Send() {
+func (c *Client) takeInput() {
 	reader := bufio.NewReader(os.Stdin)
 	for {
 		fmt.Print("> ")
@@ -218,24 +305,75 @@ func (n *Node) Send() {
 		txt := strings.TrimSpace(text)
 		if txt == "quit" {
 			return
-		} else if txt == "printMap" {
-			n.PrintLedger()
-		} else {
-			n.MessagesSent.mutex.Lock()
-			n.MessagesSent.messageMap[text] = true
-			n.Outbound <- text
-			n.MessagesSent.mutex.Unlock()
+		} else if txt == "printLedger" {
+			c.printLedger()
+		} else if txt == "printPeers" {
+			fmt.Println(c.peers)
+		} else if txt == "Transaction" {
+			t := c.RequestTransactionInfo()
+			c.BroadcastTransaction(t)
 		}
+	}
+}
+
+func (c *Client) RequestTransactionInfo() Transaction {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("From: ")
+	fmt.Print("> ")
+	from, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("formatting error: " + from)
+		return Transaction{}
+	}
+
+	fmt.Println("To: ")
+	fmt.Print("> ")
+	to, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("formatting error: " + to)
+		return Transaction{}
+	}
+
+	fmt.Println("Amount ")
+	fmt.Print("> ")
+	amount, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println("formatting error: " + amount)
+		return Transaction{}
+	}
+
+	amt, err := strconv.Atoi(amount)
+
+	if err != nil {
+		fmt.Println("formatting error: amount not an iteger")
+		return Transaction{}
+	}
+	return Transaction{From: from, To: to, Amount: amt, ID: c.getID()}
+
+}
+
+func (c *Client) PrintHostNames() {
+	// _ is convention for throwing the return value away
+	name, _ := os.Hostname()
+	addrs, _ := net.LookupHost(name)
+	fmt.Println("Name: " + name)
+
+	for indx, addr := range addrs {
+		fmt.Println("Address number " + strconv.Itoa(indx) + ": " + addr)
 	}
 }
 
 func main() {
 	// Initialize the client
 	client := makeClient()
+	client.PrintHostNames()
 	// Request IP and Port to connect to
 	ln := client.StartListen()
 	client.ConnectToNetwork()
 	go client.Listen(ln)
-	Send()
+	client.takeInput()
 
 }
